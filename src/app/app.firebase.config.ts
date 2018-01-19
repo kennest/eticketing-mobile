ccf19d63d9fc5f577345a911f9105af6fde26a70
kennest225 <kennyoulai@gmail.com>
2018-01-19 15:04:37 +0000

    first commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBBqgVyMSa9Sb6b55KCLkxbovZDojkT7ag",
+  authDomain: "e-ticketing-192610.firebaseapp.com",
+  databaseURL: "https://e-ticketing-192610.firebaseio.com",
+  projectId: "e-ticketing-192610",
+  storageBucket: "fir-auth-b0d63.appspot.com",
+  messagingSenderId: "299730430601"
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+//Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth'
+import { FIREBASE_CONFIG } from "./app.firebase.config";
+//Native
+import {OneSignal} from '@ionic-native/onesignal';
+
+@NgModule({
+  declarations: [
+    MyApp,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    OneSignal,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
